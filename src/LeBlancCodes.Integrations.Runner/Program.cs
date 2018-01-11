8021ce6eb6e163f15e0214ce62bc1574ba4ba913
Brandon LeBlanc <brandon@leblanc.codes>
2018-01-11 15:44:29 -0600

    Initialize solution

---
+++ src/LeBlancCodes.Integrations.Runner/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace LeBlancCodes.Integrations.Runner
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
